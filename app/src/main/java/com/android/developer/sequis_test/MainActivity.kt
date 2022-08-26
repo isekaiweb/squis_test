c462bd5f1be75106464506f9591a6539a2b684e2
isekaiweb <cobasaja271@gmail.com>
2022-08-26 15:25:23 +0700

    :triangular_flag_on_post: add splash screen

--- app/src/main/java/com/android/developer/sequis_test/MainActivity.kt
+++ app/src/main/java/com/android/developer/sequis_test/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.android.developer.sequis_test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        splashScreen.setKeepOnScreenCondition { false }
     }
+
+    
 }
