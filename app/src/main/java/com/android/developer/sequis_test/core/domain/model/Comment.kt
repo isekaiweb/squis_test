4cca372a3b00270e4a067ff8d3654470620da429
isekaiweb <cobasaja271@gmail.com>
2022-08-28 01:51:03 +0700

    :label: update data

--- app/src/main/java/com/android/developer/sequis_test/core/domain/model/Comment.kt
+++ app/src/main/java/com/android/developer/sequis_test/core/domain/model/Comment.kt
@@ -1,8 +1,9 @@
 package com.android.developer.sequis_test.core.domain.model
 
 data class Comment(
-    val createdAt: Long,
+    val currentTimeAt: Long,
     val pictureId: String,
     val author: String,
     val content: String,
+    val createdAt: String = "",
 )
