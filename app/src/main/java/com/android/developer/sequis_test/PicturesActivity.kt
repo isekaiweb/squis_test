a31191d4a0604222d59698e582c58b76f5c8d0a3
isekaiweb <cobasaja271@gmail.com>
2022-08-27 19:11:29 +0700

    :lipstick: update layout activity

--- app/src/main/java/com/android/developer/sequis_test/PicturesActivity.kt
+++ app/src/main/java/com/android/developer/sequis_test/PicturesActivity.kt
@@ -1,16 +1,53 @@
 package com.android.developer.sequis_test
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.android.developer.sequis_test.databinding.ActivityPicturesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PicturesActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         splashScreen.setKeepOnScreenCondition { false }
+
+        val binding = ActivityPicturesBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.topAppBar)
+        setupNavController()
+    }
+
+    private fun setupNavController(){
+
+        val navHostFragment: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        val navController = navHostFragment.navController
+
+
+        with(navController) {
+            appBarConfiguration = AppBarConfiguration(graph)
+            setupActionBarWithNavController(this, appBarConfiguration)
+        }
     }
 
 
+    override fun onOptionsItemSelected(item: MenuItem) = item.onNavDestinationSelected(
+        findNavController(R.id.nav_host_fragment)
+    ) || super.onOptionsItemSelected(item)
+
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment)
+        .navigateUp(appBarConfiguration)
 }
